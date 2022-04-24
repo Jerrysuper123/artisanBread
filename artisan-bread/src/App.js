a941367b14a765affc9648c08d1dc3cccc3454e8
Jerry <jerry.chen@trentglobal.edu.sg>
2022-04-24 12:22:25 +0000

    added nav bar structure

--- artisan-bread/src/App.js
+++ artisan-bread/src/App.js
@@ -1,9 +1,11 @@
 import Footer from "./Footer/Footer"
+import Navbar from "./NavBar/NavBar";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
      <h1>main page</h1>
      <Footer/>
     </div>
