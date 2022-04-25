7adb5a68ce43ce048a37ccc2046f88454ee8d4b9
Jerry Chen <jerryq.chen@gmail.com>
2022-04-25 10:48:59 +0800

    added nav items - shop delivery and cart item icon

--- artisan-bread/src/NavBar/NavBar.js
+++ artisan-bread/src/NavBar/NavBar.js
@@ -1,53 +1,81 @@
 import React from "react";
 import "./style.css";
-import grain from "../images/grain.png"
+import grain from "../images/grain.png";
 
-export default function Navbar(){
-    return(<React.Fragment>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand d-flex flex-column" href="#">
-    {/* <i class="fa-solid fa-bath"></i> */}
-    <img
-    className="iconImage"
-    src={grain} alt="icon"/>
-    <span className="accentThreeColor logoText">
-    Artisan
-    </span>
-    <span className="accentOneColor logoSubText">BREAD</span>
-    </a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+export default function Navbar() {
+  return (
+    <React.Fragment>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          {/* icon */}
+          <a className="navbar-brand d-flex flex-column" href="#">
+            {/* <i class="fa-solid fa-bath"></i> */}
+            <img className="iconImage" src={grain} alt="icon" />
+            <span className="accentThreeColor logoText">Artisan</span>
+            <span className="accentOneColor logoSubText">BREAD</span>
           </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-    </React.Fragment>)
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#">
+                  Shop
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Delivery
+                </a>
+              </li>
+
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <i className="fa-solid fa-user"></i>
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Login
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Register
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+            {/* cart */}
+            <section>
+              <i className="me-2 fa-solid fa-basket-shopping"></i>
+              <span className="start-100 translate-middle badge rounded-pill bg-danger">
+                3+
+                <span className="visually-hidden">unread messages</span>
+              </span>
+            </section>
+          </div>
+        </div>
+      </nav>
+    </React.Fragment>
+  );
 }
