6f04069aae3fac8a42deae94d588ab03209c9a46
Jerry Chen <jerryq.chen@gmail.com>
2022-04-28 15:19:45 +0800

    added product listing and style the card for each product

--- artisan-bread/src/App.js
+++ artisan-bread/src/App.js
@@ -25,23 +25,32 @@ function App() {
       <Navbar />
       <h1>main page</h1>
       {/* for shop page */}
-      {products.map((p) => {
-        return (
-          <div key={p.id} className="card" style={{ width: "18rem" }}>
-            {p.image_url ? (
-              <img src={p.image_url} className="card-img-top" alt={p.name} />
-            ) : null}
+      <section className="container-fluid p-3">
+        <div className="row d-flex justify-content-center gy-3 gx-3">
+          {products.map((p) => {
+            return (
+              <div key={p.id} className="card col-4" style={{ width: "18rem" }}>
+                {p.image_url ? (
+                  <img
+                    src={p.image_url}
+                    className="card-img-top"
+                    alt={p.name}
+                  />
+                ) : null}
 
-            <div className="card-body">
-              <h5 className="card-title">{p.name}</h5>
-              <p className="card-text">${p.price}</p>
-              <a href="#" className="btn btn-primary">
-                Add to cart
-              </a>
-            </div>
-          </div>
-        );
-      })}
+                <div className="card-body">
+                  <h5 className="card-title">{p.name}</h5>
+                  <p className="card-text">${p.price}</p>
+                  <a href="#" className="btn btn-primary">
+                    Add to cart
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+
       <Footer />
     </div>
   );
