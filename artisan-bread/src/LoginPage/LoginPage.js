8d9cc5e0849737b9f29ac1b2a00b278bd9fd4687
Jerry Chen <jerryq.chen@gmail.com>
2022-05-04 15:49:16 +0800

    added local storage to store accessToken

--- artisan-bread/src/LoginPage/LoginPage.js
+++ artisan-bread/src/LoginPage/LoginPage.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import axios from "axios";
+import { BASE_URL } from "../util";
+import { useEffect } from "react";
 
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const fetchAccessToken = async () => {
+    let response = await axios.post(BASE_URL + "users/login", {
+      email: email,
+      password: password,
+    });
+    console.log(response.data.accessToken);
+    setAccessToken(response.data.accessToken);
+  };
+
+  const [accessToken, setAccessToken] = useState("");
+
+  //when first load, get the accessToken in localStorage
+  useEffect(() => {
+    const accessToken = JSON.parse(localStorage.getItem("accessToken"));
+    if (accessToken) {
+      setAccessToken(accessToken);
+    }
+  }, []);
+
+  // only update accessToken when there is a new one
+  useEffect(() => {
+    localStorage.setItem("accessToken", JSON.stringify(accessToken));
+  }, [accessToken]);
+
+  //use this when user log out
+  //removeItem(): This technique is used to delete an item from localStorage based on its key.
+
   return (
     <React.Fragment>
       <main className="d-flex justify-content-center adminFormContainer align-items-center">
@@ -16,17 +57,20 @@ export default function LoginPage() {
             type="text"
             placeholder="email"
             className="form-control mt-3"
+            onChange={handleEmail}
           />
           <input
             type="text"
             placeholder="password"
             className="form-control mt-3"
+            onChange={handlePassword}
           />
           <div class="text-center">
             <input
               type="submit"
               class="mt-3 customBtn customBtnPrimary formBtn"
               value="Login"
+              onClick={fetchAccessToken}
             />
           </div>
         </div>
